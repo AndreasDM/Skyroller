d36ac2dada7401a687b720caa53091cbe7c56af4
AndreasDM <AndreasDM@users.noreply.github.com>
2018-07-30 23:30:14 +0200

    first commit, added third person ball with camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody rb;
+    private bool allowedToJump;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        allowedToJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (allowedToJump)
+                rb.AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // vi har lov til å hoppe dersom vi er på bakken
+        allowedToJump = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        allowedToJump = false;
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+    public GameObject[] tilePrefabs;
+
+    private Transform playerTransform;
+    private float spawnZlocation = 0.0f;
+    private float tileLength = 5.0f;
+    private int tilesVisible = 7;
+
+    private void Start()
+    {
+        playerTransform = GameObject.Find("Player").transform;
+    }
+
+}
