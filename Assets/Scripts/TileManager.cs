6d92c3685adeedb129789abaa30c3dbc3e90365f
AndreasDM <AndreasDM@users.noreply.github.com>
2018-07-30 23:38:52 +0200

    Added posibility to spawn tiles

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -13,6 +13,21 @@ public class TileManager : MonoBehaviour {
     private void Start()
     {
         playerTransform = GameObject.Find("Player").transform;
+
+        SpawnTile();
+        SpawnTile();
+        SpawnTile();
+    }
+
+    private void SpawnTile()
+    {
+        GameObject go;
+        go = Instantiate(tilePrefabs[0]) as GameObject;
+        go.transform.SetParent(transform); // Make tilemanager the parent of new tiles
+        // spawn new tile at spawnlocation
+        go.transform.position = Vector3.forward * spawnZlocation;
+        // set new spawn location
+        spawnZlocation += tileLength;
     }
 
 }
